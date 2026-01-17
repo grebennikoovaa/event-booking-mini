233d01d44b6f8a553ad28ec111cec3b19a1da7dc
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2026-01-17 20:19:47 +0100

    added booking badge, seats progress bar and searching

--- config/settings.py
+++ config/settings.py
@@ -58,11 +58,13 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "bookings.context_processors.booking_count",
             ],
         },
     },
