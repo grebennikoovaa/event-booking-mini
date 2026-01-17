93364a544c1cdc039432ce2ea9fdc2566f7ec37b
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2026-01-17 19:48:13 +0100

    added authpages and my booking page

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path("", views.home, name="home"),                 
+    path("", views.home, name="home"),
     path("events/", views.event_list, name="event_list"),
     path("events/<int:event_id>/", views.event_detail, name="event_detail"),
+    path("my-bookings/", views.my_bookings, name="my_bookings"),
     path("slots/<int:slot_id>/book/", views.book_slot, name="book_slot"),
     path("slots/<int:slot_id>/cancel/", views.cancel_slot, name="cancel_slot"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
+
