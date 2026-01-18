547db26a10c48311f5e81622e6851eb4188efef9
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2026-01-18 01:49:46 +0100

    added landing page and styles

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.landing, name="landing"),
     path("events/", views.event_list, name="event_list"),
     path("events/<int:event_id>/", views.event_detail, name="event_detail"),
-    path("my-bookings/", views.my_bookings, name="my_bookings"),
     path("slots/<int:slot_id>/book/", views.book_slot, name="book_slot"),
     path("slots/<int:slot_id>/cancel/", views.cancel_slot, name="cancel_slot"),
+    path("my-bookings/", views.my_bookings, name="my_bookings"),
 ]
-
